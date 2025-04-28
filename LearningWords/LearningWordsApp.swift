e857af9833aef23a0137a5e9619953b8ce680009
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-04-28 07:12:42 -0400

    Initial Commit

---
+++ LearningWords/LearningWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningWordsApp.swift
+//  LearningWords
+//
+//  Created by Dan June on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
