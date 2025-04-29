4fc3e005c9c5a3f37cac5bf8d71d1a75c2df24e1
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-04-29 11:44:05 -0400

    started on edit and removal functionality for units.

---
+++ LearningWords/CreateUnitView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateUnitView.swift
+//  LearningWords
+//
+//  Created by Dan June on 4/29/25.
+//
+
+import SwiftUI
+
+struct CreateUnitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateUnitView()
+}
---
+++ LearningWords/Units.swift
@@ -0,0 +1,8 @@
+//
+//  Units.swift
+//  LearningWords
+//
+//  Created by Dan June on 4/29/25.
+//
+
+import Foundation
